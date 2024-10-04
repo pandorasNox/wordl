95e2cbb5c2bdfd9bb8051a41110d59585d9c9dce
pandorasnox <itspandorasnox@gmail.com>
2024-10-04 09:20:54 +0200

    move getAllFilenames to test + add ecpected embeded files test

--- main.go
+++ main.go
@@ -201,22 +201,6 @@ func Map[T, U any](ts []T, f func(T) U) []U {
 	return us
 }
 
-func getAllFilenames(efs iofs.FS) (files []string, err error) {
-	if err := iofs.WalkDir(efs, ".", func(path string, d iofs.DirEntry, err error) error {
-		if d.IsDir() {
-			return nil
-		}
-
-		files = append(files, path)
-
-		return nil
-	}); err != nil {
-		return nil, err
-	}
-
-	return files, nil
-}
-
 func main() {
 	log.Println("staring server...")
 
--- main_test.go
+++ main_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	iofs "io/fs"
 	"net/url"
 	"reflect"
+	"slices"
 	"testing"
 
 	"github.com/pandorasNox/lettr/pkg/language"
@@ -267,4 +269,39 @@ func TestTemplateDataSuggest_validate(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_ExpectedEmbededFiles(t *testing.T) {
+	expectedFiles := []string{
+		"web/static/generated/main.js",
+		"web/static/generated/output.css",
+	}
+
+	embededFiles, err := getAllFilenames(fs)
+	if err != nil {
+		t.Errorf("getAllFilenames() error = %v", err)
+	}
+
+	for _, expectedFile := range expectedFiles {
+		if !slices.Contains(embededFiles, expectedFile) {
+			t.Errorf("expected embeded files, got '%v', want '%v' but was not found", embededFiles, expectedFile)
+		}
+	}
+
+}
+
+func getAllFilenames(efs iofs.FS) (files []string, err error) {
+	if err := iofs.WalkDir(efs, ".", func(path string, d iofs.DirEntry, err error) error {
+		if d.IsDir() {
+			return nil
+		}
+
+		files = append(files, path)
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return files, nil
 }
