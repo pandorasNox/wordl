5cc9d01d0fbe5afef47bb0812da0fe48da094b1b
Robert Krüger <robert.krueger@turbinekreuzberg.com>
2024-10-08 07:20:08 +0000

    move suggest routes into package

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"context"
 	"embed"
 	"errors"
 	"fmt"
 	"html/template"
-	"regexp"
 	"strings"
 	"unicode"
 
@@ -19,8 +17,6 @@ import (
 	"time"
 	"unicode/utf8"
 
-	"github.com/microcosm-cc/bluemonday"
-	"github.com/pandorasNox/lettr/pkg/github"
 	"github.com/pandorasNox/lettr/pkg/language"
 	"github.com/pandorasNox/lettr/pkg/middleware"
 	"github.com/pandorasNox/lettr/pkg/notification"
@@ -109,42 +105,6 @@ func (fd TemplateDataForm) New(l language.Language, p puzzle.Puzzle, pastWords [
 		SolutionHasDublicateLetters: solutionHasDublicateLetters,
 		ImprintUrl:                  imprintUrl,
 	}
-}
-
-type TemplateDataSuggest struct {
-	Word     string
-	Message  string
-	Language string
-	Action   string
-}
-
-var RegexpAllowedWordCharacters = regexp.MustCompile(`^[A-Za-zöäüÖÄÜß]{5}$`)
-
-var ErrFailedWordValidation = errors.New("validation failed: word is either to long, to short or contains forbidden characters")
-var ErrFailedMessageValidation = errors.New("validation failed: message contains invalid data")
-var ErrFailedActionValidation = errors.New("validation failed: action invalid")
-var ErrFailedLanguageValidation = errors.New("validation failed: language invalid")
-
-func (tds TemplateDataSuggest) validate() error {
-	if !RegexpAllowedWordCharacters.Match([]byte(tds.Word)) {
-		return ErrFailedWordValidation
-	}
-
-	p := bluemonday.UGCPolicy()
-	sm := p.Sanitize(tds.Message)
-	if sm != tds.Message {
-		return ErrFailedMessageValidation
-	}
-
-	if !slices.Contains([]string{"add", "remove"}, tds.Action) {
-		return ErrFailedActionValidation
-	}
-
-	if tds.Language != "german" && tds.Language != "english" {
-		return ErrFailedLanguageValidation
-	}
-
-	return nil
 }
 
 type keyboard struct {
@@ -385,78 +345,9 @@ func main() {
 
 	mux.HandleFunc("POST /help", routes.Help(t, &sessions, wordDb))
 
-	mux.HandleFunc("GET /suggest", func(w http.ResponseWriter, r *http.Request) {
-		err := t.ExecuteTemplate(w, "suggest", TemplateDataSuggest{})
-		if err != nil {
-			log.Printf("error t.ExecuteTemplate '/suggest' route: %s", err)
-		}
-	})
+	mux.HandleFunc("GET /suggest", routes.GetSuggest(t))
 
-	mux.HandleFunc("POST /suggest", func(w http.ResponseWriter, r *http.Request) {
-		notifier := notification.NewNotifier()
-
-		err := r.ParseForm()
-		if err != nil {
-			log.Printf("error: %s", err)
-
-			w.WriteHeader(422)
-			notifier.AddError("can not parse form data")
-			err = t.ExecuteTemplate(w, "oob-messages", notifier.ToTemplate())
-			if err != nil {
-				log.Printf("error t.ExecuteTemplate 'oob-messages': %s", err)
-			}
-			return
-		}
-
-		form := r.PostForm
-		tds := TemplateDataSuggest{
-			Word:     form["word"][0],
-			Message:  form["message"][0],
-			Language: form["language-pick"][0],
-			Action:   form["suggest-action"][0],
-		}
-
-		err = tds.validate()
-		if err != nil {
-			w.WriteHeader(422)
-			w.Header().Add("HX-Reswap", "none")
-
-			notifier.AddError(err.Error())
-			err = t.ExecuteTemplate(w, "oob-messages", notifier.ToTemplate())
-			if err != nil {
-				log.Printf("error t.ExecuteTemplate 'oob-messages' route: %s", err)
-			}
-
-			return
-		}
-
-		err = github.CreateWordSuggestionIssue(
-			context.Background(), envCfg.githubToken, tds.Word, tds.Language, tds.Action, tds.Message,
-		)
-		if err != nil {
-			w.WriteHeader(422)
-			w.Header().Add("HX-Reswap", "none")
-
-			notifier.AddError("Could not send suggestion.")
-			err = t.ExecuteTemplate(w, "oob-messages", notifier.ToTemplate())
-			if err != nil {
-				log.Printf("error t.ExecuteTemplate 'oob-messages' route: %s", err)
-			}
-
-			return
-		}
-
-		notifier.AddSuccess("Suggestion send, thank you!")
-		err = t.ExecuteTemplate(w, "oob-messages", notifier.ToTemplate())
-		if err != nil {
-			log.Printf("error t.ExecuteTemplate 'oob-messages' route: %s", err)
-		}
-
-		err = t.ExecuteTemplate(w, "suggest", TemplateDataSuggest{})
-		if err != nil {
-			log.Printf("error t.ExecuteTemplate '/suggest' route: %s", err)
-		}
-	})
+	mux.HandleFunc("POST /suggest", routes.PostSuggest(t, envCfg.githubToken))
 
 	middlewares := []func(h http.Handler) http.Handler{
 		func(h http.Handler) http.Handler {
---
+++ pkg/routes/suggest_test.go
@@ -0,0 +1,49 @@
+package routes
+
+import "testing"
+
+func TestTemplateDataSuggest_validate(t *testing.T) {
+	type fields struct {
+		Word     string
+		Action   string
+		Language string
+		Message  string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr error
+	}{
+		{name: "Suggested word match", fields: fields{Word: "gamer", Action: "add", Language: "english", Message: "test"}, wantErr: nil},
+		{name: "Suggested word match", fields: fields{Word: "GAMER", Action: "add", Language: "english", Message: "test"}, wantErr: nil},
+		{name: "Suggested word match", fields: fields{Word: "preuß", Action: "add", Language: "english", Message: "test"}, wantErr: nil},
+		{name: "Suggested word match", fields: fields{Word: "höste", Action: "add", Language: "english", Message: "test"}, wantErr: nil},
+		{name: "Suggested word match", fields: fields{Word: "hÖste", Action: "add", Language: "english", Message: "test"}, wantErr: nil},
+		{name: "Suggested word match", fields: fields{Word: "HÖSTE", Action: "add", Language: "english", Message: "test"}, wantErr: nil},
+		{name: "Suggested word match", fields: fields{Word: "fülle", Action: "add", Language: "english", Message: "test"}, wantErr: nil},
+		{name: "Suggested word match", fields: fields{Word: "FÜLLE", Action: "add", Language: "english", Message: "test"}, wantErr: nil},
+		{name: "Suggested word match", fields: fields{Word: "größe", Action: "add", Language: "english", Message: "test"}, wantErr: nil},
+		{name: "Suggested word match", fields: fields{Word: "GRÖßE", Action: "add", Language: "english", Message: "test"}, wantErr: nil},
+
+		{name: "Suggested word invalid (special chars: ?)", fields: fields{Word: "?????"}, wantErr: ErrFailedWordValidation},
+		{name: "Suggested word invalid (special chars: ô)", fields: fields{Word: "grôss"}, wantErr: ErrFailedWordValidation},
+		{name: "Suggested word invalid (special chars: emoji's (😁))", fields: fields{Word: "😁,😁,😁"}, wantErr: ErrFailedWordValidation},
+		{name: "Suggested word invalid (word to short en)", fields: fields{Word: "tiny"}, wantErr: ErrFailedWordValidation},
+		{name: "Suggested word invalid (word to short de)", fields: fields{Word: "kurz"}, wantErr: ErrFailedWordValidation},
+		{name: "Suggested word invalid (word to long en)", fields: fields{Word: "toolong"}, wantErr: ErrFailedWordValidation},
+		{name: "Suggested word invalid (word to long de)", fields: fields{Word: "zulang"}, wantErr: ErrFailedWordValidation},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tds := TemplateDataSuggest{
+				Word:     tt.fields.Word,
+				Action:   tt.fields.Action,
+				Language: tt.fields.Language,
+				Message:  tt.fields.Message,
+			}
+			if err := tds.validate(); err != tt.wantErr {
+				t.Errorf("TemplateDataSuggest.validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
