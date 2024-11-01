54c6dedf497bd4fc998d0f62b63bc377d8b6d609
pandorasnox <itspandorasnox@gmail.com>
2024-11-01 10:44:02 +0100

    fixup! move routes into router sub package

--- web/app/tailwind.config.js
+++ web/app/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // content: [],
     // content: ["/css/**/*.{html,js}"],
     // content: ["/templates/*"],
-    content: ["../pkg/routes/templates/**/*.{html,tmpl}"], //relative to where tailwind is executed
+    content: ["../pkg/router/routes/templates/**/*.{html,tmpl}"], //relative to where tailwind is executed
     safelist: [
         // 'bg-red-500',
         // 'text-3xl',
