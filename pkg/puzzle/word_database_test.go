434c5d2e9a1294d3b48bdc54cfe24e9b47b5fb04
pandorasnox <itspandorasnox@gmail.com>
2024-11-04 09:26:14 +0100

    add new worddb init tests

--- pkg/puzzle/word_database_test.go
+++ pkg/puzzle/word_database_test.go
@@ -9,24 +9,23 @@ import (
 	"github.com/pandorasNox/lettr/pkg/language"
 )
 
-func TestWordDatabase_Init(t *testing.T) {
-	all := `# metadata
-gamer
-games
-`
-	common := `# metadata
-cried
-`
-	mockFs := fstest.MapFS{
-		"all.txt": {
-			// Data: []byte("hello, world"),
-			Data: []byte(all),
-		},
-		"common.txt": {
-			Data: []byte(common),
-		},
-	}
+// func wordTxtByName(name string) []byte {
+// 	switch name {
+// 	case "case01_all":
+// 		return []byte(`# metadata
+// gamer
+// games
+// `)
+// 	case "case01_common":
+// 		return []byte(`# metadata
+// cried
+// `)
+// 	default:
+// 		return []byte{}
+// 	}
+// }
 
+func TestWordDatabase_Init(t *testing.T) {
 	type args struct {
 		fs                  iofs.FS
 		filePathsByLanguage map[language.Language]map[WordCollection][]string
@@ -39,12 +38,25 @@ cried
 	}{
 		// Add test cases.
 		{
-			name: "all collection contains common word",
+			name: "allCollection contains commonCollection word",
 			args: args{
-				fs: mockFs,
+				fs: fstest.MapFS{
+					"all.txt": {
+						// Data: []byte("hello, world"),
+						Data: []byte(`# metadata
+gamer
+games
+`),
+					},
+					"common.txt": {
+						Data: []byte(`# metadata
+cried
+`),
+					},
+				},
 				filePathsByLanguage: map[language.Language]map[WordCollection][]string{
-					language.LANG_EN: map[WordCollection][]string{
-						WC_ALL: []string{
+					language.LANG_EN: {
+						WC_ALL: {
 							"all.txt",
 						},
 						WC_COMMON: {
@@ -69,6 +81,95 @@ cried
 				},
 			},
 		},
+		//
+		{
+			name: "allCollection dedublicates word from commonCollection",
+			args: args{
+				fs: fstest.MapFS{
+					"all.txt": {
+						// Data: []byte("hello, world"),
+						Data: []byte(`# metadata
+gamer
+games
+`),
+					},
+					"common.txt": {
+						Data: []byte(`# metadata
+gamer
+`),
+					},
+				},
+				filePathsByLanguage: map[language.Language]map[WordCollection][]string{
+					language.LANG_EN: {
+						WC_ALL: {
+							"all.txt",
+						},
+						WC_COMMON: {
+							"common.txt",
+						},
+					},
+				},
+			},
+			wantErr: false,
+			wantWdb: WordDatabase{
+				Db: map[language.Language]map[WordCollection]map[Word]bool{
+					language.LANG_EN: {
+						WC_ALL: {
+							{'g', 'a', 'm', 'e', 's'}: true,
+							{'g', 'a', 'm', 'e', 'r'}: true,
+						},
+						WC_COMMON: {
+							{'g', 'a', 'm', 'e', 'r'}: true,
+						},
+					},
+				},
+			},
+		},
+		//
+		{
+			name: "Init can't open files",
+			args: args{
+				fs: fstest.MapFS{
+					"all.txt": {
+						// Data: []byte("hello, world"),
+						Data: []byte(`# metadata
+gamer
+games
+`),
+					},
+					"common.txt": {
+						Data: []byte(`# metadata
+gamer
+`),
+					},
+				},
+				filePathsByLanguage: map[language.Language]map[WordCollection][]string{
+					language.LANG_EN: {
+						WC_ALL: {
+							"non-existing-file-path",
+						},
+						WC_COMMON: {
+							"non-existing-file-path",
+						},
+					},
+				},
+			},
+			wantErr: true,
+			wantWdb: WordDatabase{
+				Db: map[language.Language]map[WordCollection]map[Word]bool{
+					language.LANG_EN: {
+						WC_ALL: {
+							{'g', 'a', 'm', 'e', 's'}: true,
+							{'g', 'a', 'm', 'e', 'r'}: true,
+						},
+						WC_COMMON: {
+							{'g', 'a', 'm', 'e', 'r'}: true,
+						},
+					},
+				},
+			},
+		},
+		//
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -76,7 +177,7 @@ cried
 			if err := wdb.Init(tt.args.fs, tt.args.filePathsByLanguage); (err != nil) != tt.wantErr {
 				t.Errorf("WordDatabase.Init() error = %v, wantErr %v", err, tt.wantErr)
 			}
-			if !reflect.DeepEqual(wdb, tt.wantWdb) {
+			if tt.wantErr == false && !reflect.DeepEqual(wdb, tt.wantWdb) {
 				t.Errorf("WordDatabase.Init() databases not equal, got %v, want %v", wdb, tt.wantWdb)
 			}
 		})
